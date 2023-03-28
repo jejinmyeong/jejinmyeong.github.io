22fdd82f9f9c128b565d574f9699c74f29df9a1b
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:28:14 +0900

    ✨ feat: add banner, contents

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import Banner from "../components/banner/banner";
+import Contents from "../components/contents/Contents";
 
 const Main: React.FC = () => {
   return (
     <div>
-      main
+      <Banner />
+      <Contents>
+        <Contents.Profile />
+        <Contents.TechStacks />
+        <Contents.Experience />
+        <Contents.Education />
+        <Contents.Project />
+        <Contents.Awards />
+      </Contents>
     </div>
   );
 };
