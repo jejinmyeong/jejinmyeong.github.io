35d8f9d20e539402b537cf516a92f41b645fa782
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:35:05 +0900

    ➕ add: modules index

---
+++ src/components/contents/modules/index.ts
@@ -0,0 +1,6 @@
+export { default as Profile } from "./Profile";
+export { default as TechStack } from "./TechStacks";
+export { default as Experience } from "./Experience";
+export { default as Education } from "./Education";
+export { default as Project } from "./Project";
+export { default as Awards } from "./Awards";
