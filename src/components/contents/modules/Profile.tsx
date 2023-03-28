345a6e5a0ae175e831a17dcdef591e1fc3bef4c2
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:34:33 +0900

    ✨ feat: profile module

---
+++ src/components/contents/modules/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { profileimg } from "assets/images";
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <img
+        className="h-32 w-32 md:h-48 md:w-48 object-cover"
+        alt="profile"
+        src={profileimg}
+      />
+      <div className="text-4xl pt-4">제진명 | Portfolio</div>
+    </div>
+  );
+};
+
+export default Profile;
