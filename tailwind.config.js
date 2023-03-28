ef4a77adfbf59712d1d587dec5dc3989a3642b4d
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:19:54 +0900

    ⚙️ edit: config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-
-      },
       fontFamily: {
-        sans: ["Jua"]
+        sans: ["Jua"],
       },
-
+      fontSize: {
+        xxs: ['0.5rem', '0.75rem']
+      }
+      ,
+      boxShadow: {
+        custom:
+          "0px 1px 2px 0px rgba(60, 64, 67, 0.3), 0px 1px 3px 1px rgba(60, 64, 67, 0.15)",
+      }
     },
   },
   plugins: [],
-}
+};
