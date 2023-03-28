73bdfea15373c912c6c3acd027930d82e61b7bea
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:33:31 +0900

    ➕ add: common component index

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+export { default as Period } from "./Period";
+export { default as Tag } from "./Tag";
+export { default as Title } from "./Title";
+export { default as WebSite } from "./WebSite";
+export { default as Logo } from "./Logo";
+export { default as HorizentalLine } from "./HorizentalLine";
+export { default as ContentsList } from "./ContentsList";
+export { default as ContentsListItem } from "./ContentsListItem";
