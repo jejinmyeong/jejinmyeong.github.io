9a923898078dce3015081dcc29afd6126531ece8
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:23:17 +0900

    ➕ add: assets index

---
+++ src/assets/images/index.ts
@@ -0,0 +1,17 @@
+export { default as banner } from "./banner.jpeg";
+export { default as profileimg } from "./profileimg.jpeg";
+export { default as ssafy } from "./ssafy.png";
+export { default as hansung } from "./hansung.png";
+export { default as drawingdream } from "./drawingdream.png";
+export { default as dotore } from "./dotore.png";
+export { default as tooliv } from "./tooliv.png";
+export { default as drawingdreamlogo } from "./drawingdreamlogo.png";
+export { default as dotorelogo } from "./dotorelogo.png";
+export { default as toolivlogo } from "./toolivlogo.png";
+export { default as project1 } from "./project1.jpg";
+export { default as project2 } from "./project2.jpg";
+export { default as project3 } from "./project3.jpg";
+export { default as hansung1 } from "./hansung1.jpg";
+export { default as hansung2 } from "./hansung2.jpg";
+export { default as portfolio } from "./portfolio.png";
+export { default as portfoliologo } from "./portfoliologo.png";
