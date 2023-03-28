8b9123d464847b6239cc83c4f4066b4334e37b8a
JINMYEONG <1995jjm@naver.com>
2023-03-29 05:34:23 +0900

    ✨ feat: experience module

---
+++ src/components/contents/modules/Experience.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ContentsTemplate from "../../template/ContentsTemplate";
+import data from "data/experience.json";
+import { ContentsList } from "components/common";
+
+const Experience = () => {
+  return (
+    <ContentsTemplate
+      title="Experience"
+      logo={{
+        type: "icon",
+        name: "💼",
+      }}
+      contents={<ContentsList list={data.list} />}
+    />
+  );
+};
+
+export default Experience;
