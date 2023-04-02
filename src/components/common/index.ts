38998ff61c62fa7c2348dd6c1e9d025ebc879fd6
JINMYEONG <1995jjm@naver.com>
2023-04-03 05:50:25 +0900

    🔥 remove: horizentalline component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,5 @@ export { default as Tag } from "./Tag";
 export { default as Title } from "./Title";
 export { default as WebSite } from "./WebSite";
 export { default as Logo } from "./Logo";
-export { default as HorizentalLine } from "./HorizentalLine";
 export { default as ContentsList } from "./ContentsList";
 export { default as ContentsListItem } from "./ContentsListItem";
