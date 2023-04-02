809d51d2db757c9153aafa3f57604c5668dd3899
JINMYEONG <1995jjm@naver.com>
2023-04-03 05:47:29 +0900

    ➕ add: type index

---
+++ src/components/common/types/index.ts
@@ -0,0 +1,8 @@
+export type {
+  default as IListComponent,
+  PeriodTypes,
+  SkillListTypes,
+  OtherWebSite,
+} from "./ListComponentTypes";
+export type { default as TitleInterface } from "./TitleTypes";
+export type { LogoComponent } from "./LogoComponentTypes";
