5ae386409c2f286dd46a6bed21bff5504fa7ddcb
JINMYEONG <1995jjm@naver.com>
2023-04-05 18:32:51 +0900

    🔨 refactor: List Component

--- src/components/common/ContentsList.tsx
+++ src/components/common/ContentsList.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { ContentsListItem } from ".";
-import { IListComponent } from "./types";
+import { IListComponent, OtherListItem, TechStackListItem } from "./types";
+import ListItemTemplate from "components/template/ListItemTemplate";
+import BadgeListItem from "./BadgeListItem";
 
-const ContentsList = ({ list }: { list: IListComponent[] }) => {
+const ContentsList = ({
+  list,
+  badge,
+}: {
+  list: IListComponent[];
+  badge?: boolean;
+}) => {
   return (
     <div>
       {list.map((data, idx) => (
-        <ContentsListItem key={idx} title={data.title} explain={data.explain} />
+        <ListItemTemplate key={idx} title={data.title} badge={badge}>
+          {badge ? (
+            <BadgeListItem explain={data.explain as TechStackListItem} />
+          ) : (
+            <ContentsListItem explain={data.explain as OtherListItem} />
+          )}
+        </ListItemTemplate>
       ))}
     </div>
   );
--- src/components/common/ContentsListItem.tsx
+++ src/components/common/ContentsListItem.tsx
@@ -1,33 +1,15 @@
 import Badge from "components/contents/techstacks/Badge";
 import React from "react";
-import { Period, Title, WebSite } from ".";
-import { IListComponent } from "./types";
+import { Period, WebSite } from ".";
+import { OtherListItem } from "./types";
 
-const ContentsListItem = ({ title, explain }: IListComponent) => {
-  const { skilllist, website, period } = explain;
+const ContentsListItem = ({ explain }: { explain: OtherListItem }) => {
+  const { website, period } = explain;
 
   return (
-    <div
-      className={`flex justify-between py-0.5 ${
-        !skilllist && "hover:bg-gray-100 px-2 rounded-md flex-col sm:flex-row"
-      }`}
-    >
-      <div className="whitespace-nowrap">
-        <Title title={title} />
-      </div>
-      {skilllist && (
-        <div className="flex gap-1 flex-wrap justify-end">
-          {skilllist.map((skill, idx) => (
-            <Badge key={idx} name={skill.name} level={skill.level} />
-          ))}
-        </div>
-      )}
-      {(website || period) && (
-        <div className="flex gap-4">
-          {website && <WebSite website={website} />}
-          {period && <Period period={period} />}
-        </div>
-      )}
+    <div className="flex gap-4">
+      {website && <WebSite website={website} />}
+      {period && <Period period={period} />}
     </div>
   );
 };
