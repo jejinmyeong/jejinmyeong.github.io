0b26ba5dcd0b886b323458cb81caeda71aa1433c
JINMYEONG <1995jjm@naver.com>
2023-05-23 23:04:29 +0900

    ✂️ edit: quatation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -7,9 +8,8 @@ module.exports = {
         sans: ["Jua"],
       },
       fontSize: {
-        xxs: ['0.5rem', '0.75rem']
-      }
-      ,
+        xxs: ["0.5rem", "0.75rem"],
+      },
       boxShadow: {
         custom:
           "0px 1px 2px 0px rgba(60, 64, 67, 0.3), 0px 1px 3px 1px rgba(60, 64, 67, 0.15)",
